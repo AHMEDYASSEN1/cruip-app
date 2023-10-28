32eba8d99dbff35e779ad3b7d77cbca9d7ae218e
Ahmed Yassen <ahmedyassen46@outlook.com>
2023-10-28 17:22:30 +0200

    two

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import "./app.scss";
+import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Website from "./Pages/Website";
 import Login from "./Pages/Login/SignIn";
 import SignUp from "./Pages/SignUp/SignUp";
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <BrowserRouter className="app">
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Website />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-        </Route>
-      </Routes>
+    <Fragment>
+      <BrowserRouter className="app">
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <Analytics/>
-    </BrowserRouter>
+    </Fragment>
   );
 }
 
